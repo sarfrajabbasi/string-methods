47097a24c4dada4c8bf30df26d3659e423816c35
sarfrajabbasi <sarfrajk96811@gmail.com>
2022-01-06 22:54:07 +0530

    string method

--- [@@iterator]().js
+++ [@@iterator]().js
@@ -3,7 +3,7 @@ var str = "sarfraj\uD835\uDC68";
 
 var strIter = str[Symbol.iterator]();
 var chartr = strIter.next()
-// console.log(chartr.value); // "sarfraj"
+console.log(chartr.value); // "sarfraj"
 // console.log(chartr.value); // "\uD835\uDC68"
 
 var str = "string";
